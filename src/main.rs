aa92f74824aa9c1139f8a026c4947abe1c0420f8
johanyim <37012949+johanyim@users.noreply.github.com>
2024-11-21 00:38:55 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::borrow::Cow;
 
 use futures::TryStreamExt;
 use rust_xlsxwriter::Workbook;
-use tiberius::{AuthMethod, Client, Config, QueryItem, QueryStream};
+use tiberius::{AuthMethod, Client, ColumnData, Config, QueryItem, QueryStream};
 use tokio::net::TcpStream;
 use tokio_util::compat::TokioAsyncWriteCompatExt;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let query = "SELECT * from claims_scr";
+    let query = "SELECT * from claims_claims";
 
     let mut config = Config::new();
     config.host("bga-azure-1.database.windows.net");
@@ -19,9 +19,7 @@ async fn main() -> anyhow::Result<()> {
     config.trust_cert(); // on production, it is not a good idea to do this
 
     let tcp = TcpStream::connect(config.get_addr()).await?;
-
     let mut client = Client::connect(config, tcp.compat_write()).await?;
-
     let mut stream = client.simple_query(query).await?;
 
     // Step 3: Prepare Excel workbook
@@ -31,8 +29,7 @@ async fn main() -> anyhow::Result<()> {
     // Step 4: Write data to the worksheet
     let mut row_index = 0;
 
-    while let Some(result_row) = stream.try_next().await? {
-        let row: QueryItem = result_row;
+    while let Some(row) = stream.try_next().await? {
         if row_index == 0 {
             // Write column headers for the first row
             for (col_index, column) in row.as_metadata().unwrap().columns().iter().enumerate() {
@@ -41,17 +38,36 @@ async fn main() -> anyhow::Result<()> {
             row_index += 1;
         }
 
-        for (col_index, columndata) in row.into_row().unwrap().into_iter().enumerate() {
-            let cell_value = match columndata {
-                tiberius::ColumnData::String(Some(val)) => val,
-                tiberius::ColumnData::I32(Some(val)) => Cow::from(val.to_string()),
-                tiberius::ColumnData::F64(Some(val)) => Cow::from(val.to_string()),
-                _ => Cow::from("Unsupported"),
-                // None => "NULL".to_string(),
-            };
-            worksheet.write_string(row_index, col_index as u16, &*cell_value)?;
+        if let Some(x) = row.into_row() {
+            for (col_index, data) in x.into_iter().enumerate() {
+                let z = match data {
+                    ColumnData::String(Some(val)) => val.to_string(),
+                    ColumnData::I16(Some(val)) => val.to_string(),
+                    ColumnData::I32(Some(val)) => val.to_string(),
+                    ColumnData::I64(Some(val)) => val.to_string(),
+                    ColumnData::U8(Some(val)) => val.to_string(),
+                    ColumnData::F32(Some(val)) => val.to_string(),
+                    ColumnData::F64(Some(val)) => val.to_string(),
+                    ColumnData::Bit(Some(val)) => val.to_string(),
+                    _ => "Unsupported".to_string(),
+                };
+                worksheet.write_string(row_index, col_index as u16, z)?;
+            }
         }
-        row_index += 1;
+
+        // for columndata in row.into_row() {
+        //     let cell_value = "NULL";
+        //
+        //     let cell_value = match columndata {
+        //         tiberius::ColumnData::String(Some(val)) => val,
+        //         tiberius::ColumnData::I32(Some(val)) => Cow::from(val.to_string()),
+        //         tiberius::ColumnData::F64(Some(val)) => Cow::from(val.to_string()),
+        //         _ => Cow::from("Unsupported"),
+        //         // None => "NULL".to_string(),
+        //     };
+        //     worksheet.write_string(row_index, col_index as u16, &*cell_value)?;
+        // }
+        // row_index += 1;
 
         // // Write data for each subsequent row
         // // for (col_index, value) in row.into_iter().enumerate() {
@@ -69,7 +85,7 @@ async fn main() -> anyhow::Result<()> {
         //         println!("{cell_value:?}");
         //     }
         // }
-        // row_index += 1;
+        row_index += 1;
     }
 
     // Step 5: Save the Excel file
